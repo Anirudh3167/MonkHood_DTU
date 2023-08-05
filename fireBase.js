2574a293102158bea5345748128b0085af08cf10
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2023-08-05 11:58:02 +0530

    done

---
+++ fireBase.js
@@ -0,0 +1,66 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js"; 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA47MdIGjExTYaP5v7DpTu8oK1O0SGlx1M",
+  authDomain: "monkhood-dc84a.firebaseapp.com",
+  databaseURL: "https://monkhood-dc84a-default-rtdb.firebaseio.com",
+  projectId: "monkhood-dc84a",
+  storageBucket: "monkhood-dc84a.appspot.com",
+  messagingSenderId: "985548987787",
+  appId: "1:985548987787:web:6043bb80d60ed79ae4238e",
+  measurementId: "G-PVX53RCM33"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+// Write action
+document.getElementById("fireBaseTrigger").addEventListener("click", async () => {
+    if (window.firebaseCommand === "create") {  // create
+        try {
+            const docRef = await addDoc(collection(db, "users"), window.userData);
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    } else if (window.firebaseCommand === "update") {   // update
+        try {
+            const userRef = await updateDoc(doc(db, "users", window.userId),window.firebaseUser);
+        } catch (err) {
+            console.log("error in firebase:");
+            console.log(err);
+        }
+    } else if (window.firebaseCommand === "delete") {   // delete
+        try {
+            const userRef = await deleteDoc(doc(db, "users", window.userId));
+        } catch (err) {
+            console.log("error in firebase:");
+            console.log(err);
+        }
+    }
+    window.firebaseCommand = null;
+})
+
+// Read action
+addEventListener("load", async () => {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    var myData = [];
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} =>`);
+        console.log(doc.data())
+        myData.push({doc.id : doc.data()});
+    });
+    window.myData = myData;
+});
