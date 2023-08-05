3ef32fa6b7f9e24ba9345a3cfd8479e98d099019
Anirudh <77240291+Anirudh3167@users.noreply.github.com>
2023-08-05 12:04:14 +0530

    Update fireBase.js

--- fireBase.js
+++ fireBase.js
@@ -9,14 +9,7 @@ import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "https://
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA47MdIGjExTYaP5v7DpTu8oK1O0SGlx1M",
-  authDomain: "monkhood-dc84a.firebaseapp.com",
-  databaseURL: "https://monkhood-dc84a-default-rtdb.firebaseio.com",
-  projectId: "monkhood-dc84a",
-  storageBucket: "monkhood-dc84a.appspot.com",
-  messagingSenderId: "985548987787",
-  appId: "1:985548987787:web:6043bb80d60ed79ae4238e",
-  measurementId: "G-PVX53RCM33"
+ // Your local config
 };
 
 // Initialize Firebase
